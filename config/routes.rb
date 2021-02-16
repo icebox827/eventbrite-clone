dcfd834400581484ca89cbd9469204c3142ff714
icebox827 <icebox827@gmail.com>
2021-02-16 18:20:05 -0500

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/user'
+  resources :users, only: %i[new create show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
