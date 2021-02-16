f244a79eebbf30feae7c4d78fca04dbefaaa4d05
icebox827 <icebox827@gmail.com>
2021-02-16 18:34:17 -0500

    Update methods in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,29 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    redirect_to user_path(current_user) if logged_in?
   end
 
   def create
-    @user = User.new
-
-    
+    @user = User.create(sign_up_params)
+    if @user.save
+      flash[:notice] = 'User created successfully'
+      session[:user_id] = @user.id
+      session[:username] = @user.name
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = 'User not created, please try again'
+      render 'new'
+    end
   end
 
   def show
-    @user = User.all
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name)
   end
 end
